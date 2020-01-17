fcc920195d0d8b343f14d2b3af8a8e1b5cb42ea6
Josh Kahn <jkahn@amazon.com>
2020-01-17 13:58:14 -0600

    Remove need for docker

---
+++ iOS/AWS News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AWS News
+//
+//  Created by Kahn, Josh on 1/17/20.
+//  Copyright © 2020 Kahn, Josh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
