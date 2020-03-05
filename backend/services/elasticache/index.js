671df883a5458d55c7d6123934ba0d48aaa1d334
Josh Kahn <jkahn@amazon.com>
2020-03-05 10:51:46 -0600

    Fixes + support for popular articles query

--- backend/services/elasticache/index.js
+++ backend/services/elasticache/index.js
@@ -13,6 +13,7 @@
 const Redis = require("ioredis");
 
 const LATEST_CONTENT_KEY = process.env.LATEST_CONTENT_KEY;
+const POPULAR_CONTENT_KEY = process.env.POPULAR_CONTENT_KEY;
 const BLOG_COUNT_KEY = process.env.BLOG_COUNT_KEY;
 
 let redis = new Redis.Cluster([
@@ -22,6 +23,11 @@ let redis = new Redis.Cluster([
   }
 ]);
 
+/**
+ * Retrieves listing of latest articles ids and sets nextToken.
+ * @param {Int} start 
+ * @param {Int} limit 
+ */
 async function getLatestArticles(start, limit) {
   try {
     const pipeline = redis.pipeline();
@@ -33,13 +39,23 @@ async function getLatestArticles(start, limit) {
     // get the total length of the list to determine if we need to paginate
     pipeline.llen(LATEST_CONTENT_KEY);
 
-    const [ articleIds, length ] = await pipeline.exec();
+    const result = await pipeline.exec();
+    const [ err1, articleIds ] = result[0];
+    const [ err2, length ] = result[1];
+    
+    if (err1 || err2) {
+      console.error(`[ERROR - lrange] ${err1}`);
+      console.error(`[ERROR - llen] ${err2}`);
+      return {
+        error: "An error has occurred retrieving latest articles"
+      }
+    }
 
-    const nextId = length > end ? end + 1 : null;
+    const nextIndex = length > end ? end + 1 : null;
 
     return {
       ids: articleIds,
-      nextToken: nextId ? _encodeNextToken("Article", nextId) : ""
+      nextToken: nextIndex ? _encodeNextToken("Article", nextIndex) : ""
     };
   } catch(error) {
     console.error(JSON.stringify(error));
@@ -47,11 +63,61 @@ async function getLatestArticles(start, limit) {
   }
 }
 
+/**
+ * Retrieves listing of popular article ids and set nextToken if more.
+ * @param {Int} start 
+ * @param {Int} limit 
+ */
+async function getPopularArticles(start, limit) {
+  try {
+    const pipeline = redis.pipeline();
+
+    // get the leaderboard for articles in the desired range
+    const end = start + limit - 1;
+    pipeline.zrevrange(POPULAR_CONTENT_KEY, start, end);
+
+    // get the total number of articles in the leaderboard
+    pipeline.zcount(POPULAR_CONTENT_KEY, 0, "+inf");
+
+    const result = await pipeline.exec();
+    const [ err1, articleIds ] = result[0];
+    const [ err2, length ] = result[1];
+    
+    if (err1 || err2) {
+      console.error(`[ERROR - lrange] ${err1}`);
+      console.error(`[ERROR - llen] ${err2}`);
+      return {
+        error: "An error has occurred retrieving latest articles"
+      }
+    }
+
+    // going to max out at 50 popular articles, to avoid potentially being never ending
+    const nextIndex = Math.min(length, 50) > end ? end + 1 : null;
+
+    return {
+      ids: articleIds,
+      nextToken: nextIndex ? _encodeNextToken("Article", nextIndex) : ""
+    };
+  } catch(error) {
+    console.error(JSON.stringify(error));
+    return { error: error.message };
+  }
+}
+
+/**
+ * Decodes nextToken opaque token to retrieve next item index.
+ * @param {String} nextToken 
+ */
 function _decodeNextToken(nextToken) {
   let str = new Buffer(nextToken, "base64").toString("ascii");
   return str.split(":")[1];
 }
 
+/**
+ * Encodes an opaque token for nextToken value.
+ * @param {String} type 
+ * @param {Int} nextIndex 
+ */
 function _encodeNextToken(type, nextIndex) {
   return new Buffer(`${type}:${nextIndex}`).toString("base64");
 }
@@ -71,7 +137,7 @@ exports.handler = async(event) => {
     case "latestArticles":
       return await getLatestArticles(start, limit);
     case "popularArticles":
-      return [];
+      return await getPopularArticles(start, limit);
     default:
       throw("No such method");
   }
