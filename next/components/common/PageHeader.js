17a33771c551edb88df1860fc64a5bb63be32155
Josh Kahn <jkahn@amazon.com>
2020-06-17 16:49:03 -0500

    (fix) title

--- next/components/common/PageHeader.js
+++ next/components/common/PageHeader.js
@@ -2,7 +2,7 @@ export default function PageHeader({ title }) {
   return (
     <header className="bg-white border-b border-gray-300">
       <div className="py-6 px-4 sm:px-6 lg:px-8">
-        <h1 className="text-2xl font-bold leading-tight text-gray-900 sm:text-4xl">
+        <h1 className="text-2xl font-bold leading-tight text-gray-900 capitalize sm:text-4xl">
           { title || '' }
         </h1>
       </div>
