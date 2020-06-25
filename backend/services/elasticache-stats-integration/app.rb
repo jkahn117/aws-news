199b84823e9c72c08783db9588fc0bf7f3b31e39
Josh Kahn <jkahn@amazon.com>
2020-06-25 17:05:17 -0500

    (feat) build out integration for statistics

--- backend/services/elasticache-stats-integration/app.rb
+++ backend/services/elasticache-stats-integration/app.rb
@@ -1,50 +1,74 @@
-# async function getArticleMetrics() {
-#   const pipeline = redis.pipeline();
-#   // get total
-#   pipeline.get(`${ARTICLE_COUNT_KEY}:total`);
-#   // get days on which we have new articles
-#   pipeline.zrevrange(`${ARTICLE_COUNT_KEY}:days`, 0, 6);
-#   const result = await pipeline.exec();
-
-#   const [ err1, totalCount ] = result[0];
-#   const [ err2, days ] = result[1];
-
-#   if (err1 || err2) {
-#     console.error(`[ERROR] ${err1}`);
-#     console.error(`[ERROR] ${err2}`);
-#     return {
-#       error: "An error has occurred retrieving articles"
-#     }
-#   }
-
-#   for (let date of days) {
-#     pipeline.get(`${ARTICLE_COUNT_KEY}:${date}`);
-#   }
-
-#   let dailyCounts = await pipeline.exec();
-
-#   return {
-#     total: totalCount,
-#     dailyCounts: days.reduce((n, d, i) => { n[d] = dailyCounts[i]; return n }, {})
-#   }
-# }
+#
+# ElastiCacheStatisticsIntegrationFunction
+# 
+# This function allows AWS AppSync to interoperate with Amazon
+# ElastiCache (Redis). It provides a set of actions that correspond
+# to data retrievals from ElastiCache, specific to Statistics.
+#
 
 require 'redis'
 require 'aws-xray-sdk/lambda'
 
-redis = Redis.new(cluster: [ "redis://#{ENV['ELASTICACHE_ENDPOINT']}:#{ELASTICACHE_PORT}" ])
+@redis = nil
 
 ARTICLE_COUNT_KEY = ENV['ARTICLE_COUNT_KEY']
-BLOG_COUNT_KEY = ENV['ARTICLE_COUNT_KEY']
+BLOG_COUNT_KEY = ENV['BLOG_COUNT_KEY']
 
+#
+# Get Article statistics. Response:
+#
+# {
+#   "total": "1",
+#   "daily": {
+#     "20200625": "1"
+#   }
+# }
+#
 def get_article_stats
-  total = redis.get "#{ARTICLE_COUNT_KEY}:total"
-
-  {
-    total: total
-  }
+  get_stats_for(ARTICLE_COUNT_KEY)
 end
 
+def get_blog_stats(blogId)
+  get_stats_for("#{BLOG_COUNT_KEY}:#{blogId}")
+end
+
+#
+# Main handler method.
+#
 def handler(event:, context:)
-  get_article_stats
+  unless @redis
+    begin
+      @redis = Redis.new(cluster: [ "redis://#{ENV['ELASTICACHE_ENDPOINT']}:#{ENV['ELASTICACHE_PORT']}" ])
+    rescue Exception => e
+      puts e.message
+      puts e.backtrace.inspect
+    end
+  end
+  
+  case event['action']
+  when 'getArticleStats'
+    get_article_stats
+  when 'getBlogStats'
+    get_blog_stats(event['args']['blogId'])
+  else
+    raise "[ERROR] Unknown method - #{event['action']}, failing"
+  end
+end
+
+private
+
+def get_stats_for(key)
+  total, days = @redis.pipelined do
+    @redis.get "{#{key}}:total"
+    @redis.zrevrange("{#{key}}:days", 0, 6)
+  end
+  
+  daily_counts = @redis.pipelined do
+    days.each { |d| @redis.get("{#{key}}:#{d}") }
+  end
+  
+  {
+    total: total || 0,
+    daily: Hash[days.zip(daily_counts)]
+  }
 end
