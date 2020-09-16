49a18ccbacb223c50efe8ae334599f14443ba627
Josh Kahn <jkahn@amazon.com>
2020-09-16 15:30:23 -0500

    placeholder for loading image

--- next/components/ui/LazyImage.js
+++ next/components/ui/LazyImage.js
@@ -1,10 +1,18 @@
-import { useEffect, useRef, useState } from 'react';
-
 import LazyLoad from 'react-lazyload';
+
+function Placeholder() {
+  return (
+    <div className="border border-gray-300 shadow rounded-md p-4 max-w-sm w-full mx-auto">
+      <div className="animate-pulse flex space-x-4">
+        <div className="h-full w-full bg-gray-400"/>
+      </div>
+    </div>
+  );
+}
 
 export default function LazyImage({ src, srcset, sizes, alt, className }) {
   return (
-    <LazyLoad once offset={ 50 }>
+    <LazyLoad once offset={ 50 } placeholder={ <Placeholder /> }>
       <img src={ src }
         srcSet={ srcset }
         sizes={ sizes }
